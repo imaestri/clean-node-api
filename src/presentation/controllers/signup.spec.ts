47927c063a6fa314127610d8b8d8c1534544f28d
imaestri <isaquemaestri01@gmail.com>
2022-08-04 18:46:13 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup';
+
+describe('SignUp Controller', () => {
+  it('should return 400 if no name is provided', () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				email: 'any_email@gmail.com',
+				password: 'any_password',
+				password_confirmation: 'any_password_confirmation'
+			}
+		}
+		const httpResponse = sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+  });
+});
+
