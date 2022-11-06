76bc7d54644963f9ed722efd7cd624aa93786a1f
imaestri <isaquemaestri01@gmail.com>
2022-11-05 23:16:47 -0300

    refactor: move all error imports to a sigle file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
+import { InvalidParamError, MissingParamError, ServerError } from '../errors'
 import { EmailValidator } from '../protocols/email-validator'
 import { SignUpController } from './signup'
 
