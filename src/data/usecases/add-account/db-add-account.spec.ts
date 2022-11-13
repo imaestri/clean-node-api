efe695eae2c68ce48c70f9b0d52f281061808bc3
imaestri <isaquemaestri01@gmail.com>
2022-11-13 11:53:25 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Encrypter } from '../../protocols/encrypter'
+import { Encrypter } from './db-add-account-protocols'
 import { DbAddAccount } from './db-add-account'
 
 interface SutTypes {
