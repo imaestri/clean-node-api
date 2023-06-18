3e0127cbf317178125a1201857f6a27667b37319
imaestri <isaquemaestri01@gmail.com>
2023-06-18 17:28:28 -0300

    refactor: removing parameters that are no longer used

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,13 +4,10 @@ export const MongoHelper = {
 	client: null as MongoClient,
 
 	async connect(uri: string): Promise<void> {
-		this.client = await MongoClient.connect(process.env.MONGO_URL, {
-			useNewUrlParser: true,
-      useUnifiedTopology: true,
-		})
+		this.client = await MongoClient.connect(process.env.MONGO_URL)
 	},
 
-	async disconnect(): Promise<void>{
+	async disconnect(): Promise<void> {
 		await this.client.close()
 	}
 }
