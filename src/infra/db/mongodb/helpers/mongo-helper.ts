eb0c3e447fe320034c6fe08bbc9ec95afacc6f42
imaestri <isaque.maestri@minu.co>
2023-07-20 09:12:25 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
@@ -13,5 +14,14 @@ export const MongoHelper = {
 
 	getCollection (name: string): Collection {
 		return this.client.db().collection(name)
+	},
+
+	map: (collection: any): any => {
+		const { _id, ...collectionWithoutId } = collection;
+		const account = Object.assign({}, collectionWithoutId, {
+			id: _id.toHexString(),
+		})
+
+		return account;
 	}
 }
